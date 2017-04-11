784abee141016ccb70be596e2ac472273bdb6720
Martin Etmajer <martin@etmajer.com>
2017-04-12 00:01:42 +0200

    Update helloworld-msa app to latest version.

--- apps/helloworld-msa/deploy.sh
+++ apps/helloworld-msa/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
-OS_PROJECT="${1:-helloworld-msa}"
+OS_PROJECT="${1:-redhat-helloworld-msa}"
 
-# Currently needed for zipkin-mysql, since volume mounting will only be added in 1.5.0 for clusters of type 'oc cluster up'
-oc login -u system:admin --insecure-skip-tls-verify
-oc adm policy add-scc-to-user anyuid -z default -n "${OS_PROJECT}"
+if [ -z "${OS_MASTER_IP}" ]; then
+  echo "Error: the public IP of the OpenShift master must be provided via the OS_MASTER_IP environment variable."
+  exit 1
+fi
 
 oc login -u developer -p developer --insecure-skip-tls-verify
 oc new-project "${OS_PROJECT}" --description="The Red Hat HelloWorld MSA (Microservice Architecture)." || true
@@ -11,24 +12,22 @@ oc project "${OS_PROJECT}"
 oc policy add-role-to-user admin "system:serviceaccount:${OS_PROJECT}:turbine"
 
 sed -i.bak "s/value: \"OS_PROJECT\"/value: \"$OS_PROJECT\"/" "${OS_PROJECT}.yml"
-sed -i.bak "s/value: \"OS_MASTER_IP\"/value: \"$OS_MASTER_IP\"/" "${OS_PROJECT}.yml"
-sed -i.bak "s/value: \"OS_PUBLIC_HOSTNAME\"/value: \"$OS_PUBLIC_HOSTNAME\"/" "${OS_PROJECT}.yml"
+sed -i.bak "s/value: \"OS_SUBDOMAIN\"/value: \"$OS_MASTER_IP.nip.io\"/" "${OS_PROJECT}.yml"
 oc create -f "${OS_PROJECT}.yml"
 
 sed -i.bak "s/value: \"OS_PROJECT\"/value: \"$OS_PROJECT\"/" "${OS_PROJECT}-with-zipkin.yml"
-sed -i.bak "s/value: \"OS_MASTER_IP\"/value: \"$OS_MASTER_IP\"/" "${OS_PROJECT}-with-zipkin.yml"
-sed -i.bak "s/value: \"OS_PUBLIC_HOSTNAME\"/value: \"$OS_PUBLIC_HOSTNAME\"/" "${OS_PROJECT}-with-zipkin.yml"
+sed -i.bak "s/value: \"OS_SUBDOMAIN\"/value: \"$OS_MASTER_IP.nip.io\"/" "${OS_PROJECT}-with-zipkin.yml"
 oc create -f "${OS_PROJECT}-with-zipkin.yml"
 
 if [ -n "${OS_PULL_DOCKER_IMAGES}" ]; then
-  sudo docker pull docker.io/metmajer/hystrix-dashboard:1.0.26.1
-  sudo docker pull docker.io/metmajer/turbine-server:1.0.26.1
-  sudo docker pull docker.io/metmajer/msa-aloha
-  sudo docker pull docker.io/metmajer/msa-api-gateway
-  sudo docker pull docker.io/metmajer/msa-bonjour
-  sudo docker pull docker.io/metmajer/msa-frontend
-  sudo docker pull docker.io/metmajer/msa-hola
-  sudo docker pull docker.io/metmajer/msa-ola
-  sudo docker pull docker.io/mysql:5.7
-  sudo docker pull docker.io/openzipkin/zipkin:1.20
+  sudo docker pull fabric8/turbine-server:1.0.28
+  sudo docker pull fabric8/hystrix-dashboard:1.0.28
+  sudo docker pull gencatcloud/mysql-openshift:5.7
+  sudo docker pull metmajer/redhatmsa-frontend
+  sudo docker pull metmajer/redhatmsa-api-gateway
+  sudo docker pull metmajer/redhatmsa-aloha
+  sudo docker pull metmajer/redhatmsa-bonjour
+  sudo docker pull metmajer/redhatmsa-hola
+  sudo docker pull metmajer/redhatmsa-ola
+  sudo docker pull openzipkin/zipkin:1.22
 fi
