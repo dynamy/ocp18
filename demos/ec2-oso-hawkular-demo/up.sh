7bd4d0cace38bb530e756aba826b53da56cd0de8
Martin Etmajer <martin@etmajer.com>
2017-04-24 21:07:57 +0200

    Add idempotency to sed expressions.

--- demos/ec2-oso-hawkular-demo/up.sh
+++ demos/ec2-oso-hawkular-demo/up.sh
@@ -25,8 +25,11 @@ sudo apt-get install -y docker-engine=1.12.3-0~xenial
 sudo service docker start
 
 # Install docker: prepare for OpenShift
-sudo sed -i 's/ExecStart=\(.*\)/ExecStart=\1 --insecure-registry 172.30.0.0\/16/' /lib/systemd/system/docker.service
-sudo sed -i 's/SocketMode=\(.*\)/SocketMode=0666/' /lib/systemd/system/docker.socket
+if ! grep -e '--insecure-registry 172.30.0.0/16' /lib/systemd/system/docker.service &>/dev/null; then
+  sudo sed -i 's/ExecStart=\(.*\)/ExecStart=\1 --insecure-registry 172.30.0.0\/16/' /lib/systemd/system/docker.service
+fi
+
+sudo sed -i 's/SocketMode=\(.*\)/SocketMode=0666/' /lib/systemd/system/docker.socketsudo sed -i 's/SocketMode=\(.*\)/SocketMode=0666/' /lib/systemd/system/docker.socket
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
