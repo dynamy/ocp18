5a75d04c1e083808b235dffebd838f6f93302fb4
dynamy <myrvin.yap@dynatrace.com>
2018-06-17 15:31:41 +0800

    Update new.sh

--- environment/new.sh
+++ environment/new.sh
@@ -66,21 +66,4 @@ oc project openshift-infra
 oc create serviceaccount dynatrace
 oc adm policy add-scc-to-user privileged -z dynatrace
 
-cd /home/ec2-user/ocp18/apps
-
-# Install OpenShift 'easytravel' application template
-OS_PROJECT=easytravel
-pushd "${OS_PROJECT}"
-oc adm policy add-scc-to-user anyuid -z default -n "${OS_PROJECT}"
-oc login "${loginserver}" -u developer -p developer --insecure-skip-tls-verify
-oc new-project "${OS_PROJECT}" --description="The Dynatrace easyTravel sample application." || true
-oc project "${OS_PROJECT}"
-oc create -f "${OS_PROJECT}"-with-loadgen.yml
-if [ -n "${OS_PULL_DOCKER_IMAGES}" ]; then
-        sudo docker pull dynatrace/easytravel-mongodb
-        sudo docker pull dynatrace/easytravel-backend
-        sudo docker pull dynatrace/easytravel-frontend
-        sudo docker pull dynatrace/easytravel-nginx
-        sudo docker pull emperorwilson/easytravel-loadgen-dt
-fi
-popd
+su - ec2-user -c "./start.sh"
