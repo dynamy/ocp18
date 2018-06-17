386a66eb5743222da93b06fbd231abf10c24866b
dynamy <myrvin.yap@dynatrace.com>
2018-06-17 15:44:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ export OS_PULL_DOCKER_IMAGES="true"
 
 # SET env var
 OS_PUBLIC_HOSTNAME="${OS_PUBLIC_HOSTNAME:-$OS_PUBLIC_IP}"
+export loginserver=`echo "https://${OS_PUBLIC_HOSTNAME}:8443"`
 
 cd /home/ec2-user/ocp18/apps
 
@@ -13,7 +14,7 @@ cd /home/ec2-user/ocp18/apps
 OS_PROJECT=easytravel
 pushd "${OS_PROJECT}"
 oc adm policy add-scc-to-user anyuid -z default -n "${OS_PROJECT}"
-oc login -u developer -p developer --insecure-skip-tls-verify
+oc login "${loginserver}" -u developer -p developer --insecure-skip-tls-verify
 oc new-project "${OS_PROJECT}" --description="The Dynatrace easyTravel sample application." || true
 oc project "${OS_PROJECT}"
 oc create -f "${OS_PROJECT}"-with-loadgen.yml
